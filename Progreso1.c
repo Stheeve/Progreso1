fdc517f47cf04d352cdd1ec3b1f159f0eff2b9a0
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2023-10-24 15:14:52 -0500

    Se agrego un for como contador

--- Progreso1.c
+++ Progreso1.c
@@ -2,11 +2,23 @@
 int main(){
 
 int num;
-float aux2;
+float aux;
+
+
 int i=0;
+// Variables que se van a utilizar en el ejercicio
 
 printf("Coloque un numero entero\n");
 scanf("%d",&num);
+
+// Se incluyo un for el cual nos va a servir como contador
+for (int i = 0; i < num; i++)
+{
+ aux=3.141632/i;
+
+ printf("la respuesta es %f\n", aux );
+}
+
 
 
 }
