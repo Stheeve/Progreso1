f2d28fc732d05244759cf3c3975b8a531ce6717b
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2023-10-24 15:17:07 -0500

    Se agrego un if

--- Progreso1.c
+++ Progreso1.c
@@ -3,21 +3,31 @@ int main(){
 
 int num;
 float aux;
-
-
 int i=0;
 // Variables que se van a utilizar en el ejercicio
 
+
+
+
+
 printf("Coloque un numero entero\n");
 scanf("%d",&num);
-
-// Se incluyo un for el cual nos va a servir como contador
+// If para evitar la entrada de numeros negativos
+if (num>0)
+{
+    // Se incluyo un for el cual nos va a servir como contador
 for (int i = 0; i < num; i++)
 {
  aux=3.141632/i;
 
  printf("la respuesta es %f\n", aux );
 }
+}
+else {
+    printf("Debe colocar un numero positivo");
+}
+
+
 
 
 
